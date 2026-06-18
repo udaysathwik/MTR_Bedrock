4cc33c2c86354e94d97dbe64d051e7366ec4058d
udaysathwik <rithwikkuli@gmail.com>
2026-06-18 17:58:04 +0530

    Create LanguageDisplay.js

---
+++ behaviour_pack/configs/LanguageDisplay.js
@@ -0,0 +1,22 @@
+// behavior_packs/mod/config/LanguageDisplay.js
+
+const LanguageDisplay = {
+
+    NORMAL: {
+        id: "normal",
+        name: "Normal"
+    },
+
+    CJK_ONLY: {
+        id: "cjk_only",
+        name: "CJK Only"
+    },
+
+    NON_CJK_ONLY: {
+        id: "non_cjk_only",
+        name: "Non-CJK Only"
+    }
+
+};
+
+export default LanguageDisplay;
