264e6585076a3fac7d15d05a46cb2839fae1ba22
udaysathwik <rithwikkuli@gmail.com>
2026-06-18 17:44:28 +0530

    Create VehicleExtention.js

---
+++ behaviour_pack/data/VehicleExtention.js
@@ -0,0 +1,18 @@
+simulate(deltaTime) {
+    // TODO: route announcements
+    // TODO: signal blocking
+    // TODO: train sensors
+    // TODO: platform stopping
+}
+
+playMotorSound(...) {
+    // TODO
+}
+
+playDoorSound(...) {
+    // TODO
+}
+
+dispose() {
+    this.persistentVehicleData?.dispose();
+}
